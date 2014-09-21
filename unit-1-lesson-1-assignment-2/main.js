8a92bc5e8440a8c90c0388ec5a9f3d62d244dc5f
carl sednaoui <carlsednaoui@gmail.com>
2014-09-21 19:05:06 -0400

    Complete hwk for unit-1-lesson-1-assignment-2

---
+++ unit-1-lesson-1-assignment-2/main.js
@@ -0,0 +1,45 @@
+function calculate(e) {
+  
+  // get vars for convenience
+  var numberOne = parseInt(document.getElementById('number-one').value);
+  var numberTwo = parseInt(document.getElementById('number-two').value);
+
+  // int check
+  if (isNaN(numberOne) || isNaN(numberTwo)) {
+    alert('X and Y need to be numbers');
+    e.preventDefault();
+    e.stopPropagation();
+    return;
+  }
+
+  var task = e.currentTarget.attributes['data-operation'].value;
+  var taskSymbol;
+  var operation;
+
+  if (task === 'add') {
+    taskSymbol = '+'
+    operation = numberOne + numberTwo;
+  } else {
+    taskSymbol = '-'
+    operation = numberOne - numberTwo;
+  }
+
+  var result = [
+    numberOne,
+    taskSymbol,
+    numberTwo,
+    '=',
+    operation].join(' ');
+
+  // add response to DOM
+  document.getElementById('result').innerHTML = result;
+
+  // prevent form submission
+  e.preventDefault();
+  e.stopPropagation();
+
+}
+
+// attach listeners
+document.getElementById('add-numbers').addEventListener('click', calculate, false);
+document.getElementById('subtract-numbers').addEventListener('click', calculate, false);
