91253ca85aa4ebefb550a6e491a6c251fbf4852c
carl sednaoui <carlsednaoui@gmail.com>
2014-09-30 08:36:41 -0400

    Refactor input logic

--- unit-1-lesson-3-assignment-3/main.js
+++ unit-1-lesson-3-assignment-3/main.js
@@ -1,4 +1,63 @@
 angular.module('myApp', [])
   .controller('madLib', function($scope) {
-    console.log('hello from the madLib controller');
+    $scope.inputs = {};
+
+    $scope.models = [
+      {
+        model: 'maleName',
+        placeholder: 'male name',
+        type: 'text'
+      },
+      {
+        model: 'jobTitle',
+        placeholder: 'job title',
+        type: 'text'
+      },
+      {
+        model: 'tediousTask',
+        placeholder: 'tedious task',
+        type: 'text'
+      },
+      {
+        model: 'dirtyTask',
+        placeholder: 'dirty task',
+        type: 'text'
+      },
+      {
+        model: 'celebrity',
+        placeholder: 'celebrity',
+        type: 'text'
+      },
+      {
+        model: 'uselessSkill',
+        placeholder: 'useless skill',
+        type: 'text'
+      },
+      {
+        model: 'obnoxiousCelebrity',
+        placeholder: 'obnoxious celebrity',
+        type: 'text'
+      },
+      {
+        model: 'hugeNumber',
+        placeholder: 'huge number',
+        type: 'text'
+      },
+      {
+        model: 'adjective',
+        placeholder: 'adjective',
+        type: 'text'
+      }
+    ];
+
+    $scope.getGender = function(word) {
+      if (word == 'hisHer') {
+        return ($scope.gender == 'male') ? 'his' : 'her';
+      }
+
+      if (word == 'heShe') {
+        return ($scope.gender == 'male') ? 'he' : 'she';
+      }
+    }
+
   });
