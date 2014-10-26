5206eebb27dac1627356082f2df6126fcc2931ed
carl sednaoui <carlsednaoui@gmail.com>
2014-10-26 15:29:24 -0400

    Continues work on Mad lib refactor

--- unit-1-lesson-3-mad-lib-refactor-2/main.js
+++ unit-1-lesson-3-mad-lib-refactor-2/main.js
@@ -1,4 +1,59 @@
 angular.module('myApp', [])
   .controller('madLib', function($scope) {
-    console.log('hello from the madLib controller');
+    $scope.data = {};
+
+    // $scope.textInputs = [
+    //   {
+    //     model: 'maleName',
+    //     placeholder: 'male name'
+    //   },
+    //   {
+    //     model: 'jobTitle',
+    //     placeholder: 'job title'
+    //   },
+    //   {
+    //     model: 'tediousTask',
+    //     placeholder: 'tedious task'
+    //   },
+    //   {
+    //     model: 'dirtyTask',
+    //     placeholder: 'dirty task'
+    //   },
+    //   {
+    //     model: 'celebrity',
+    //     placeholder: 'celebrity name'
+    //   },
+    //   {
+    //     model: 'uselessSkill',
+    //     placeholder: 'useless skill'
+    //   },
+    //   {
+    //     model: 'obnoxiousCelebrity',
+    //     placeholder: 'obnoxious celebrity'
+    //   },
+    // ];
+
+    $scope.verifyGender = function(gender, word) {
+      // bumper
+      if ( gender === 'male' ) return;
+
+      // swap word
+      if ( word === 'he'  ) return 'she';
+      if ( word === 'his' ) return 'her';
+      if ( word === 'him' ) return 'her';
+    }
+
+    $scope.submit = function() {
+      debugger;
+      if ($scope.generateMadLib.$valid) {
+        $scope.state = 'display';  
+      } else {
+
+      }
+    }
+
+    $scope.reset = function() {
+      $scope.data = {};
+      $scope.state = 'create';
+    }
   });
