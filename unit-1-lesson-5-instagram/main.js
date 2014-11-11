2ddc2bb78433bc7fc2185b43e5db5e588069aa45
carl sednaoui <carlsednaoui@gmail.com>
2014-11-10 22:37:24 -0500

    Complete lesson 5

--- unit-1-lesson-5-instagram/main.js
+++ unit-1-lesson-5-instagram/main.js
@@ -1,7 +1,40 @@
 angular.module('myApp', ['ngAnimate'])
-  .controller('instaSearch', function($scope) {
+  .controller('instaSearch', function($scope, $http) {
     $scope.searchInstagram = function() {
-      console.log($scope.search);
-    }
+      var url = 'https://api.instagram.com/v1/tags/'
+              + $scope.search
+              + '/media/recent';
 
+      var query = {
+        client_id: 'd5b1dcbfb2bf4f3096d1a346ae2ae2ea',
+        callback: 'JSON_CALLBACK'
+      };
+      
+      $http({
+        url: url,
+        method: 'JSONP',
+        params: query
+      })
+        .success(function(res) {
+          if (res.meta.error_type) {
+            $scope.resultMessage = res.meta.error_message;
+            return;
+          }
+          
+          var prevSearch = $scope.search;
+          var len = res.data.length;
+
+          $scope.search = '';
+          $scope.resultMessage = 'We found '
+                               + len
+                               + ' results for "'
+                               + prevSearch
+                               + '"';
+
+          $scope.images = res.data;
+        })
+        .error(function() {
+          $scope.resultMessage = 'There was an error, please try again.';
+        })
+    }
   });
