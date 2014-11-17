faccde9db6e9b1d584728c7773027a9864655de4
carl sednaoui <carlsednaoui@gmail.com>
2014-11-16 21:15:14 -0500

    Add files for own based on 2.1.2

---
+++ unit-2-lesson-1-assignment-1/app/owm-app.js
@@ -0,0 +1,37 @@
+angular.module('OWMApp', ['ngRoute'])
+  .value('owmCities', 
+          ['New York', 'Dallas', 'Chicago'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl : './home.html',
+            controller : 'HomeCtrl'
+        }).when('/cities/:city', {
+            templateUrl : './city.html',
+            controller : 'CityCtrl',
+            resolve : {
+                city: function(owmCities, $route, $location) {
+                    var city = $route.current.params.city;
+                    if(owmCities.indexOf(city) == -1 ) {
+                        $location.path('/error');
+                        return;
+                    }
+                    return city;
+                }
+            }
+        }).when('/error', {
+            template : '<p>Error Page Not Found</p>'
+        }).otherwise({
+          redirectTo : '/error'
+        })
+    })
+    .run(function($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path('/error');
+        });
+    })
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope, city) {
+        $scope.city = city;
+    });
