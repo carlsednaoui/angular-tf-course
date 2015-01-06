11819929782fec62d2e305aaee81f55741b54a35
carl sednaoui <carlsednaoui@gmail.com>
2015-01-06 09:04:27 -0500

    Add 'editable' directive with THT

--- unit-3-1-2/app.js
+++ unit-3-1-2/app.js
@@ -56,3 +56,27 @@ app.directive('megaVideo', function($sce) {
     }
   }
 });
+
+app.directive('editable', function() {
+  return {
+    restrict: 'E',
+    templateUrl: './editable.html',
+    scope: {
+      edit: '='
+    },
+  }
+})
+
+app.factory('editMode', function() {
+  return {
+    mode: false
+  };
+})
+
+app.controller('editMe', function($scope, editMode) {
+  $scope.edit = editMode;
+})
+
+app.controller('sidebar', function($scope, editMode) {
+  $scope.edit = editMode;
+})
